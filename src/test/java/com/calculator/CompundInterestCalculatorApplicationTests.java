a891f25f59c4f4b0a7dbfeae28bf0e39de9fd89c
Fernando Antonio <fernando.ant22@gmail.com>
2022-08-08 10:37:26 -0300

    Initial Commit

---
+++ src/test/java/com/calculator/CompundInterestCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompundInterestCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
